273b03e47f687d9b5d5898c626a4ff68bf4b294f
narayan jha <narayan@opoyi.com>
2023-11-18 01:30:46 +0530

    first rout practice

---
+++ index.js
@@ -0,0 +1,11 @@
+// better comments vs code extension
+//? heading information etc
+//* Highlight
+//! Errors
+// Todos 
+// Rainbow brackets vs code extension
+(function name(params) {
+    console.log(params);
+})();
+var myName = "Narayan Kumar Jha";
+console.log(myName); // Corrected: Log the variable myName.
---
+++ index.ts
@@ -0,0 +1,13 @@
+// better comments vs code extension
+//? heading information etc
+//* Highlight
+//! Errors
+// Todos 
+
+
+// Rainbow brackets vs code extension
+(function name(params) {
+    console.log(params)
+})()
+var myName: string = "Narayan Kumar Jha";
+console.log(myName); // Corrected: Log the variable myName.
